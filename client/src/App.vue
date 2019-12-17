631c304a4e535235e494519d06540c18fc6ed407
ChayanonPiakhuntod <chayanonb5814664@gmail.com>
2019-12-17 19:19:27 +0700

    close #4 - Do Ambulance Frontend

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+     
+    <v-app-bar app class="orange darken-2">
+      <v-toolbar-title class="headline text-uppercase" >
+        <span>AMBULANCE</span>
+
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+      <v-content>
+          <router-view />
+      </v-content>
+  </v-app>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data() {
+            return {
+            };
+        },
+        methods: {
+            login() {
+                this.$router.push("/");
+            }
+        }
+    };
+</script>
